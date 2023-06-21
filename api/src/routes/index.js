bed18abdb780eef205b8b7873837e2b3dc9de095
cesarsell <cesar_ab05@hotmail.com>
2023-06-20 23:49:48 -0300

    second commit

---
+++ api/src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+const mainRouter = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+mainRouter.use("/", );
+
+
+module.exports = mainRouter;

c0940568e7c8f0b9ce3f6ec72e09f56d42032d8c
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-06-22 15:20:16 -0300

    fix errores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Stack from 'react-bootstrap/Stack'
+
 function App() {
 
   return (
     <div className='container'>
-    <Stack direction="horizontal" gap={3}>
+    
       <div className="p-2">NameStore</div>
       <div className="p-2 ms-auto">Products</div>
       <div className="vr" />
       <div className="p-2">Log in</div>
       <div className="p-2">Sign un</div>
-    </Stack>
+    
   </div>
   )
 }

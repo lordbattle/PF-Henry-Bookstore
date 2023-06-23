16426d3558ddbd36043b2eb0024259680583e60e
lucas <robledolucas42@gmail.com>
2023-06-22 21:14:29 -0300

    Landing hecho, nav prototipo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Stack from 'react-bootstrap/Stack'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Nav from './components/Nav/Nav'
 import LandingPage from './components/LandingPage/LandingPage'
@@ -13,7 +12,7 @@ function App() {
 
   return (
     <div className='container'>
-      <Stack direction="horizontal" gap={3}>
+      
 
         { pathname !== '/' && <Nav />}
 
@@ -23,13 +22,6 @@ function App() {
         <Route path='/about' element={<About />}></Route>
         <Route path='/login' element={<Login />}></Route>
       </Routes>
-
-      {/* <div className="p-2">NameStore</div>
-      <div className="p-2 ms-auto">Products</div>
-      <div className="vr" />
-      <div className="p-2">Log in</div>
-      <div className="p-2">Sign un</div> */} 
-      </Stack>
   </div>
   )
 }

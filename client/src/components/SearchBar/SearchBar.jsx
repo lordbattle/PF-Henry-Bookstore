4a63ce1853b555862f3445ce891e0d06a90c5e3d
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-06-23 20:05:33 -0300

    SearchBar terminado, sin funcionalidad

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,12 @@
+import style from '../SearchBar/SearchBar.module.css'
 
 const SearchBar = () => {
     return (
-        <div>
-            <h1>SearchBar</h1>
+        <div className={style.main}>
+            <div className={style.container}>
+                <input type='search' className={style.input} placeholder="Ingresa el libro que deseas buscar"></input>
+                <button className={style.btn}>🔍︎</button>
+            </div>
         </div>
     )
 }

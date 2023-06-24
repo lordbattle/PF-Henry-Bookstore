9337eb121d18e2de273551ffe172888f7757bd3b
lucas <robledolucas42@gmail.com>
2023-06-24 13:06:28 -0300

    cards renderizando al back

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -4,14 +4,24 @@ const GET_BOOKS = "GET_BOOKS";
 export const getBooks=()=>{
     return async (dispatch)=>{
         try {
-            const {data} = await axios.get("https://www.googleapis.com/books/v1/volumes?startIndex=1&maxResults=40&q=%20+title&key=AIzaSyB1opBkQMWTSQmL77Uiin6L35qJhWNhHas");
-            console.log("LOG DATA ACTIONS", data.items)
+            const {data} = await axios.get("http://localhost:3001/books");
+            console.log("LOG DATA ACTIONS", data)
             return dispatch({
                 type: GET_BOOKS,
-                payload: data.items
+                payload: data
             })
         } catch (error) {
             alert(`ERROR DEL CATCH ${error}` )
+        }
+    }
+}
+
+export const getBookById = (idBook) =>{
+    return async (dispatch)=>{
+        try {
+            const {data} = await axios.get(`http://localhost:3001/books/${idBook}`)
+        } catch (error) {
+            alert(`Error catch getbooksbyid ${error}`)
         }
     }
 }

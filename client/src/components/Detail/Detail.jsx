c0c8b95ffa910ec56be6a41d4441d13e10c0b809
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-06-24 20:40:25 -0300

    2

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useParams, Link} from "react-router-dom"
-import { getBookById } from "../../redux/actions";
+import { getBookById, deleteBook } from "../../redux/actions";
 
 const Detail = () =>{
     const {id} = useParams();

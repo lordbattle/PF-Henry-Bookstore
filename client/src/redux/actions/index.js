9e633423d06d4a1782a0164de4a80ff84c5d04af
lucas <robledolucas42@gmail.com>
2023-06-24 13:32:30 -0300

    actions get book by id hecha

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const GET_BOOKS = "GET_BOOKS";
+const GET_BOOK_ID= "GET_BOOK_ID"
 
 export const getBooks=()=>{
     return async (dispatch)=>{
@@ -19,7 +20,12 @@ export const getBooks=()=>{
 export const getBookById = (idBook) =>{
     return async (dispatch)=>{
         try {
-            const {data} = await axios.get(`http://localhost:3001/books/${idBook}`)
+            const {data} = await axios.get(`http://localhost:3001/books/${idBook}`);
+            console.log("LOG DEL GETID", data)
+            return dispatch({
+                type: GET_BOOK_ID,
+                payload: data
+            })
         } catch (error) {
             alert(`Error catch getbooksbyid ${error}`)
         }

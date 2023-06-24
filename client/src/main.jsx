b0c9701edc34a1b3c7cacb343be475e71ff402b0
chioveh <109402226+chioveh@users.noreply.github.com>
2023-06-24 18:45:45 -0300

    fix bugs auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
 import { BrowserRouter } from 'react-router-dom'
-import { Auth0provider } from '@auth0/auth0-react'
+// import { Auth0provider } from '@auth0/auth0-react'
 import { Provider } from 'react-redux'
 import {store} from "./redux/store/index.js"
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN
+// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-  <BrowserRouter>
-    <React.StrictMode>
-      <Auth0provider 
-      domain={domain} 
-      clientId={clientId} 
-      redirectUri={window.location.origin}>
-        <App />
-      </Auth0provider>
-    </React.StrictMode>
-  </BrowserRouter>
-</Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        {/* <Auth0provider 
+        domain={domain} 
+        clientId={clientId} 
+        redirectUri={window.location.origin}> */}
+          <App />
+        {/* </Auth0provider> */}
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 )

079b948843c49a80ef4b229185866f03a6f2a0c7
diegohansselperez <diegohansselp24@gmail.com>
2023-06-24 18:48:28 -0700

    Footer is moving

--- api/src/db.js
+++ api/src/db.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
+require('dotenv').config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
+
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT } = process.env;
+
 
 const sequelize = new Sequelize(
   `${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,

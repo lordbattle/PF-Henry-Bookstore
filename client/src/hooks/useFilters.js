f017c77a02b2b43107cb6b0cdbbb1a052f13f3e0
diegohansselperez <diegohansselp24@gmail.com>
2023-06-24 20:52:38 -0700

    useFilters in progress...

--- client/src/hooks/useFilters.js
+++ client/src/hooks/useFilters.js
@@ -1,20 +1,25 @@
-const { useState } = require("react")
+import  { useState } from "react";
 
 const useFilters = () => {
-    const [filters, setFilters] = useState({
-        rating: "all",
-        price: "all",
-        genre: "all"
-    })
+  const [filters, setFilters] = useState({
+    rating: "all",
+    price: "all",
+    genre: "all",
+  });
+
+  const filtersBooks = (arr) => {
+    return arr.filter((book) => {
+      return (
+        filters.genre === "all" ||
+        (book.genre === filters.genre && filters.rating === "all") ||
+        (book.usersRating === filters.rating && filters.price === "all") ||
+        book.price === filters.price
+      );
+    });
+  };
+
+  return {setFilters, filtersBooks}
+};
 
 
-    const filtersBooks = (arr) => {
-        return arr.filter(book => {
-            return (
-                filters.genre === "all" || book
-            )
-        })
-
-    }
-
-}
+export default useFilters;

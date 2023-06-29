7984ff891e774ad039a6f28371307424f229f549
lucas <robledolucas42@gmail.com>
2023-06-29 13:11:50 -0300

    set y get localStorage cart hecho

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,10 +1,45 @@
 
 import { useDispatch } from "react-redux"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getBooks } from "../../redux/actions"
 import Cards from "../Cards/Cards"
 import style from '../Card/Card.module.css'
 const Card = ({ currentBooks }) => {
+
+  //LOCALSTORAGE PARA EL CARRITO
+  const [cart, setCart]= useState([])
+
+  useEffect(()=>{
+    //almacena en el localstorage
+    localStorage.setItem("cart", JSON.stringify(cart))
+  })
+
+  useEffect(()=>{
+    ///LEE el carrito almacenado en localstorage
+    const storedCart = localStorage.getItem("cart");
+    if(storedCart){
+      setCart(JSON.parse(storedCart))
+    }
+  }, [])
+
+  const addToCart = (newItem)=>{
+    const existingItem = cart.find((item)=> item.id === newItem.id);
+    if(existingItem){
+    const updatedCart = cart.map((item)=>{
+      if(item.id === newItem.id){
+        return {
+          ...item,
+          stock: item.stock + 1,
+        }
+      }
+      return item
+    })
+      setCart(updatedCart);
+  } else {
+    setCart([...cart, newItem]);
+  }
+}
+/////////////////////////////////
 
   const dispatch = useDispatch()
   useEffect(() => {
@@ -27,7 +62,7 @@ const Card = ({ currentBooks }) => {
               imageLinks={v.bookPic}
               price={v.price}
               stock={v.stock}
-
+              addToCart={addToCart}
             // Otras propiedades de volumeInfo que desees utilizar
             />
           );

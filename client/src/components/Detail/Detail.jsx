b7ee235a90316076f7c98f5115c9696ea8e9f0b2
lucas <robledolucas42@gmail.com>
2023-06-28 23:04:45 -0300

    edit product hecho

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -18,25 +18,43 @@ const Detail = () =>{
     const {active, authors, averageRating, bookPic, description, genre, identifier, pages, publishedDate, publisher, subtitle, title, userRating, price, stock} = details;
    
     const [editedProduct, setEditedProduct]=useState({
-        title: '',
-        subtitle: '',
-        price: 0,
-        stock: 0,
-        authors: '',
-        description: '',
-        bookPic: '',
-        averageRating: 0,
-        genre: '',
-        pages: 0,
-        publishedDate: '',
-        publisher: '',
-        identifier: '',
+        title: title,
+        subtitle: subtitle,
+        price: price,
+        stock: stock,
+        authors: authors,
+        description: description,
+        bookPic: bookPic,
+        averageRating: averageRating,
+        genre: genre,
+        pages: pages,
+        publishedDate: publishedDate,
+        publisher: publisher,
+        identifier: identifier,
 
     })
+    
+    useEffect(() => {
+        setEditedProduct({
+          title: title,
+          subtitle: subtitle,
+          price: price,
+          stock: stock,
+          authors: authors,
+          description: description,
+          bookPic: bookPic,
+          averageRating: averageRating,
+          genre: genre,
+          pages: pages,
+          publishedDate: publishedDate,
+          publisher: publisher,
+          identifier: identifier
+        });
+      }, [title, subtitle, price, stock, authors, description, bookPic, averageRating, genre, pages, publishedDate, publisher, identifier]);
 
     const [isEditing, setIsEditing]=useState(false)
-    const handleInputChange = (e)=>{
 
+    const handleInputChange = (e)=>{
         const {name, value} = e.target;
         setEditedProduct({...editedProduct, [name]: value})
     }
@@ -44,7 +62,8 @@ const Detail = () =>{
     const handleEditProduct=(e)=>{
         e.preventDefault();
         dispatch(editBook(id, editedProduct));
-        setIsEditing(false)
+        setIsEditing(false);
+        //window.location.reload();
     }
 
     return (
@@ -53,12 +72,15 @@ const Detail = () =>{
                 <div className={style.detail}>
                 <h2>ID DEL LIBRO: {id}</h2>
                 <h4>{active===true? ( <button onClick={()=>{dispatch(deleteBook(id));}} className={style.deleteButtom}>Deshabilitar producto</button> ):(<button onClick={()=>(dispatch(activeBook(id)))} className={style.deleteButtom}>Habilitar producto</button>)}</h4>
-                <h4><button onClick={()=>setEditedProduct(true)} className={style.deleteButtom}>Edit product</button></h4>
+                <h4><button onClick={()=>setIsEditing(true)} className={style.deleteButtom}>Edit product</button></h4>
                 </div>
 
             <div className='d-flex p-3'>
             <img src={bookPic} alt="Imagen del libro" width='35%'/>
             <div className={style.advancedDetail}>
+
+           {isEditing && (
+                <>
             <input
             placeholder="TITLE"
                 type="text"
@@ -128,6 +150,12 @@ const Detail = () =>{
                 value={editedProduct.description}
                 onChange={handleInputChange}
             />
+            <button onClick={handleEditProduct}>Update product</button>
+            <button onClick={()=>setIsEditing(false)}>Cancel update</button>
+            </>)}
+            
+           {!isEditing && (
+            <>
             <h1 style={{width: '100%',fontSize: '30px'}}>{title}</h1>
             <h2 style={{fontSize: '20px'}}>{subtitle}</h2>
             <p style={{fontSize: '20px'}}>Price: ${price}</p>
@@ -139,7 +167,9 @@ const Detail = () =>{
             <p style={{width: '33.3%', fontSize: '18px'}}>Pages: {pages}</p>
             <p style={{width: '33.3%', fontSize: '18px'}}>{publisher} : {publishedDate}</p>
             <p style={{fontSize: '20px'}}>ISBN : {identifier}</p>
-            
+            </>
+           )}
+
             </div>
             </div>
             <Link to="/home" style={{textDecoration: 'none'}}><p className={style.back}>Go back</p></Link>

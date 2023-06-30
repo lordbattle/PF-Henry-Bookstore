b593d7695ac0ce04c8af3d3032efc26a0faac47c
lucas <robledolucas42@gmail.com>
2023-06-29 21:03:11 -0300

    localStorage global

---
+++ client/src/components/LocalStorage/LocalStorage.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+export const useStorage = () =>{
+
+    const [cart, setCart]= useState(()=>{
+        try {
+          //lee el localStorage si tuviese info, sino array vacio
+          const storedCart = localStorage.getItem("cart");
+          return storedCart ? JSON.parse(storedCart) : []
+       }
+        catch (error) {
+         console.log(`ERROR DEL getItem de storedCart ${error}`);
+         return [];
+       }
+      })
+    
+      useEffect(()=>{
+        //almacena en el localstorage
+        localStorage.setItem("cart", JSON.stringify(cart))
+      },[cart])
+    
+    
+      const addToCart = (newItem)=>{
+        const existingItem = cart.find((item)=> item.id === newItem.id);
+        if(existingItem){
+        const updatedCart = cart.map((item)=>{
+          if(item.id === newItem.id){
+            return {
+              ...item,
+              stock: item.stock + 1,
+            }
+          }
+          return item
+        })
+          setCart(updatedCart);
+      } else {
+        setCart([...cart, newItem]);
+      }
+    }
+    return {cart, addToCart}
+}
+export default useStorage;

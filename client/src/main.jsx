c1f0f570e050a0abfc6ffc707c3fd577b6505b45
chioveh <109402226+chioveh@users.noreply.github.com>
2023-06-30 17:58:25 -0300

    auth0 fixed and working

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,9 +8,6 @@ import { Provider } from 'react-redux'
 import {store} from "./redux/store/index.js"
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-// const { REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID } = process.env;
-// const REACT_APP_AUTH0_DOMAIN = 'bookstores.us.auth0.com';
-// const REACT_APP_AUTH0_CLIENT_ID = 'Beeh8eJn498o8IAm5tq0itAgqhqtI35m';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
@@ -19,8 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Auth0Provider 
         domain={domain}
         clientId={clientId}
-        redirectUri={window.location.origin}
-        >
+        redirectUri={window.location.origin}>
           <App />
         </Auth0Provider>
       </BrowserRouter>

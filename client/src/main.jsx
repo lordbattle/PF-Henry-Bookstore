257ef71e0c7a322c794c7137b4efad922de96b20
chioveh <109402226+chioveh@users.noreply.github.com>
2023-06-30 17:57:35 -0300

    auth0 fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,16 +6,20 @@ import { BrowserRouter } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { Provider } from 'react-redux'
 import {store} from "./redux/store/index.js"
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 // const { REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID } = process.env;
+// const REACT_APP_AUTH0_DOMAIN = 'bookstores.us.auth0.com';
+// const REACT_APP_AUTH0_CLIENT_ID = 'Beeh8eJn498o8IAm5tq0itAgqhqtI35m';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
         <Auth0Provider 
-        // domain={REACT_APP_AUTH0_DOMAIN}
-        // clientId={REACT_APP_AUTH0_CLIENT_ID}
-        // redirectUri={window.location.origin}
+        domain={domain}
+        clientId={clientId}
+        redirectUri={window.location.origin}
         >
           <App />
         </Auth0Provider>

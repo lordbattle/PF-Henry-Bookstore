a36f00f982c317aaff71e7f63c1ce0928bdd5c67
lucas <robledolucas42@gmail.com>
2023-06-29 21:16:48 -0300

    trayendo los datos del localStorage

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,15 @@
-import { useState, useEffect } from "react";
 
+import localStorage from '../LocalStorage/LocalStorage'
 
 export const Cart =()=>{
+const {cart} = localStorage();
+
+    console.log("ENTRE AL LOCALSTORAGE POR CART", cart)
+
+    return(
+        <div>
+            <h1>CART</h1>
+        </div>
+    )
 }
 export default Cart;

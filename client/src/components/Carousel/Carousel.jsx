1c4a0c4a05b6b34c3778419ea060dfc8697eb0d0
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-07-03 16:56:55 -0300

    carrusel agregado

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,31 +1,36 @@
 import Carousel from 'react-bootstrap/Carousel';
-
+import carousel1 from '../../images/carru_1.jpg';
+import carousel2 from '../../images/carrusell_2.jpg';
+import carousel3 from '../../images/carrusell_3.jpg';
 
 const CarouselComp = () => {
     return (
         <Carousel>
           <Carousel.Item interval={3000}>
             <img
-              className="d-block w-100" height={'350vh'}
-              src=""
+              className="d-block w-100" height={'250vh'}
+              src={carousel1}
               alt="First slide"
             />
           </Carousel.Item>
           <Carousel.Item interval={3000}>
             <img
-              className="d-block w-100"
-              src=""
+              className="d-block w-100" height={'250vh'}
+              src={carousel2}
               alt="Second slide"
             />
           </Carousel.Item>
+
           <Carousel.Item interval={3000}>
             <img
-              className="d-block w-100"
-              src=""
+              className="d-block w-100" height={'250vh'}
+              src={carousel3}
               alt="Third slide"
             />
           </Carousel.Item>
-          <Carousel.Item interval={3000}>
+
+          {/* /////////////////DESCOMENTAR SI SE VAN A USAR ///////////////////*/}
+          {/* <Carousel.Item interval={3000}>
             <img
               className="d-block w-100"
               src=""
@@ -38,7 +43,7 @@ const CarouselComp = () => {
               src=""
               alt="First slide"
             />
-          </Carousel.Item>
+          </Carousel.Item> */}
         </Carousel>
       );
 }

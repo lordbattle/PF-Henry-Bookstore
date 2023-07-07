d0039b2735c13b97f86d0febe2f1af0b847f6e1e
cesarsell <cesar_ab05@hotmail.com>
2023-07-07 05:54:05 -0300

    fix

--- api/src/controllers/usersControllers.js
+++ api/src/controllers/usersControllers.js
@@ -217,14 +217,11 @@ const registerUser = async (data) => {
 //------|  PUT/  |---------->
 const putUser = async (id, updatedData) => {
   try {
-
     if (updatedData.email) {
-
       const salt = bcrypt.genSaltSync();
       const hashedEmail = bcrypt.hashSync(updatedData.email, salt);
       updatedData.email = hashedEmail;
     }
-
 
     const [updatedRowsCount] = await User.update(updatedData, {
       where: { id: id },
@@ -235,10 +232,7 @@ const putUser = async (id, updatedData) => {
     if (updatedRowsCount[0] === 0) {
       throw new Error("There is no user with the specified id");
     }
-
-
-      return updatedRowsCount;
-    }
+    return updatedRowsCount;
   } catch (e) {
     throw new Error(e.message);
   }

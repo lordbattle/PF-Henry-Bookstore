fac48b9950adbe2f7048ba5a5b7e382d592db131
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-07-11 01:09:33 -0300

    lisandro_estilos

--- client/src/components/Dashboard/componentDashboard/SalesTable/SalesTable.jsx
+++ client/src/components/Dashboard/componentDashboard/SalesTable/SalesTable.jsx
@@ -5,6 +5,22 @@ import style from "./SalesTable.module.css"
 
 
 const SalesTable = () => {
+
+  let orderStatus = (order) => {
+    switch (order) {
+      case "approved":
+        return <span style={{backgroundColor: '#00C851', color: 'white', borderRadius: '5px', padding: '0px 10px'}}>APPROVED</span>
+        break;
+      case "pending":
+        return <span style={{backgroundColor: '#33b5e5', color: 'white', borderRadius: '5px', padding: '0px 10px'}}>PENDING</span>
+        break;
+      case "reject":
+        return <span style={{backgroundColor: '#ff4444', color: 'white', borderRadius: '5px', padding: '0px 10px'}}>REJECT</span>
+        break;
+      default:
+        break;
+    }
+  }
         const dispatch = useDispatch()
 
        const orders = useSelector(state => state.orders)
@@ -34,10 +50,14 @@ const SalesTable = () => {
             </thead>
             <tbody>
               {arrayOrders?.map((order) => (
+                
+
+
+
                   <tr key={order.id}>
                     <td>{order.id}</td>
                     <td>${order.total}.00</td>
-                    <td className='text-uppercase'>{order.status}</td>
+                    <td className='text-uppercase'>{orderStatus(order.status)}</td>
                     <td>{order.userId}</td>
                     <td>{order.orderItems.length}</td>
                     <td>{order.dueDate.split('T')[0]}</td>

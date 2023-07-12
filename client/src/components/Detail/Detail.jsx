cc3cd8c58d81295329f150b8a7ba83508096de2e
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2023-07-12 16:55:37 -0300

    correciones

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -219,7 +219,7 @@ const Detail = () =>{
             <p style={{fontSize: '20px'}}>Stock: {stock}</p>
             <p style={{fontSize: '20px'}}>Author's: {authors}</p>
             <p style={{fontSize: '20px', maxHeight: '60%', borderRadius: '20px', paddingLeft: '8px', backgroundColor: '#71a6e2'}}>Description: {description}</p>
-            <p style={{width: '50%', fontSize: '20px'}}>Global rating: {averageRating}</p> <p style={{width: '50%', fontSize: '20px'}}>User rating: {userRating}</p>
+            <p style={{width: '50%', fontSize: '20px'}}>Global rating: {averageRating}</p> <p style={{width: '50%', fontSize: '20px'}}></p>
             <p style={{width: '33.3%', fontSize: '18px'}}>Categories: {genre}</p>
             <p style={{width: '33.3%', fontSize: '18px'}}>Pages: {pages}</p>
             <p style={{width: '33.3%', fontSize: '18px'}}>{publisher} : {publishedDate}</p>

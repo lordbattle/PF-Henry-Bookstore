0ffdee530b48b65f58c70b8cf4187bc0aa00459d
cesarsell <cesar_ab05@hotmail.com>
2023-07-13 15:23:14 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, '0.0.0.0', () => {
     console.log(`listening at port ${PORT}`);
   });

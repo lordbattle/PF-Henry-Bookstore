a197606ffc2fca18f6dcfd7afea71964020801e5
cesarsell <cesar_ab05@hotmail.com>
2023-07-13 03:53:44 -0300

    Fix edtibook dashboar

--- client/src/components/Dashboard/componentDashboard/BookManager/BookManager.jsx
+++ client/src/components/Dashboard/componentDashboard/BookManager/BookManager.jsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
-import { getBookById, deleteBook, activeBook, editBook } from "../../../../redux/actions";
-import style from '../BookManager/BookManager.module.css';
+import {
+  getBookById,
+  deleteBook,
+  activeBook,
+  editBook,
+} from "../../../../redux/actions";
+import style from "../BookManager/BookManager.module.css";
 import Swal from "sweetalert2";
 
 const BookManager = () => {
@@ -16,15 +21,27 @@ const BookManager = () => {
   const details = useSelector((state) => state.details);
   console.log("log del details", details);
 
-  const { active, authors, averageRating, bookPic, description, genre, identifier, pages, publishedDate, publisher, subtitle, title, userRating, price, stock } = details;
+  const {
+    active,
+    authors,
+    averageRating,
+    bookPic,
+    description,
+    genre,
+    identifier,
+    pages,
+    publishedDate,
+    publisher,
+    subtitle,
+    title,
+    userRating,
+    price,
+    stock,
+  } = details;
 
-  const handleDeleteBook = async() => {
-    await Swal.fire(
-      'The product was deleted',
-      '',
-      'error'
-    )
-    dispatch(deleteBook(id))
+  const handleDeleteBook = async () => {
+    await Swal.fire("The product was deleted", "", "error");
+    dispatch(deleteBook(id));
     window.location = "/dashboard";
   };
 
@@ -85,9 +102,20 @@ const BookManager = () => {
 
   const handleEditProduct = (e) => {
     e.preventDefault();
-    dispatch(editBook(id, editedProduct));
+
+    const editedProductCopy = { ...editedProduct }; // Crear una copia del objeto editedProduct
+
+    // Eliminar las propiedades vacías o no completadas del objeto editedProductCopy
+    Object.keys(editedProductCopy).forEach((key) => {
+      if (editedProductCopy[key] === "") {
+        delete editedProductCopy[key];
+      }
+    });
+
+    dispatch(editBook(id, editedProductCopy));
     setIsEditing(false);
-    //window.location.reload();
+
+    window.location.reload();
   };
 
   return (
@@ -95,147 +123,211 @@ const BookManager = () => {
       <div className={style.container}>
         <div className={style.detail}>
           <h2>ID DEL LIBRO: {id}</h2>
-          <h4>
+          <h5>
             {active === true ? (
-              <button onClick={() => { dispatch(deleteBook(id)); }} className={style.deleteButton}>Deshabilitar producto</button>
+              <button
+                onClick={() => {
+                  dispatch(deleteBook(id));
+                  setTimeout(() => {
+                    window.location.reload();
+                  }, 0);
+                }}
+                className={style.deleteButton}
+              >
+                Disable product
+              </button>
             ) : (
-              <button onClick={() => (dispatch(activeBook(id)))} className={style.deleteButton}>Habilitar producto</button>
+              <button
+                onClick={() => {
+                  dispatch(activeBook(id));
+                  setTimeout(() => {
+                    window.location.reload();
+                  }, 0);
+                }}
+                className={style.deleteButton}
+              >
+                Enable product
+              </button>
             )}
-          </h4>
+          </h5>
           <div className={style.buttonGroup}>
-            <button onClick={() => setIsEditing(true)} className={style.editButton}>Edit product</button>
-            <button onClick={handleDeleteBook} className={style.deleteButton}>Delete Book</button>
+            <button
+              onClick={() => setIsEditing(true)}
+              className={style.editButton}
+            >
+              Edit product
+            </button>
+            {/* <button onClick={handleDeleteBook} className={style.deleteButton}>
+              Delete Book
+            </button> */}
           </div>
         </div>
 
-        <div className='d-flex p-3'>
+        <div className="d-flex p-3">
           <img src={bookPic} alt="Imagen del libro" className={style.img} />
           <div className={style.advancedDetail}>
-
             {isEditing && (
               <div className="d-flex flex-column">
-                <label style={{ fontSize: '23px' }}>Title:
+                <label style={{ fontSize: "23px" }}>
+                  Title:
                   <input
                     placeholder="TITLE"
                     type="text"
                     name="title"
                     value={editedProduct.title}
                     onChange={handleInputChange}
-                    style={{ minWidth: '20rem' }}
+                    style={{ minWidth: "20rem" }}
                   />
                 </label>
-                <label style={{fontSize: '20px'}}>Subtitle:
-                <input
-                placeholder="SUBTITLE"
+                <label style={{ fontSize: "20px" }}>
+                  Subtitle:
+                  <input
+                    placeholder="SUBTITLE"
                     type="text"
                     name="subtitle"
                     value={editedProduct.subtitle}
                     onChange={handleInputChange}
-                    style={{minWidth: '20rem'}}
-                />
-            </label>
-            <label style={{fontSize: '18px'}}>Price:
-                <input
-                placeholder="PRICE"
+                    style={{ minWidth: "20rem" }}
+                  />
+                </label>
+                <label style={{ fontSize: "18px" }}>
+                  Price:
+                  <input
+                    placeholder="PRICE"
                     type="number"
                     name="price"
                     value={editedProduct.price}
                     onChange={handleInputChange}
-                    style={{maxWidth: '5rem'}}
-                />
-            </label>
-            <label style={{fontSize: '18px'}}>Stock:
-                <input
-                placeholder="STOCK"
+                    style={{ maxWidth: "5rem" }}
+                  />
+                </label>
+                <label style={{ fontSize: "18px" }}>
+                  Stock:
+                  <input
+                    placeholder="STOCK"
                     type="number"
                     name="stock"
                     value={editedProduct.stock}
                     onChange={handleInputChange}
-                    style={{maxWidth: '3.5rem'}}
-                />
-            </label>
-            <label style={{fontSize: '18px'}}>Author's:
-                <input
-                placeholder="AUTHOR'S"
+                    style={{ maxWidth: "3.5rem" }}
+                  />
+                </label>
+                <label style={{ fontSize: "18px" }}>
+                  Author's:
+                  <input
+                    placeholder="AUTHOR'S"
                     type="text"
                     name="authors"
                     value={editedProduct.authors}
                     onChange={handleInputChange}
-                    style={{minWidth: '20rem'}}
-                />
-            </label>
-            <label style={{fontSize: '18px'}}>Genres:
-                <input
-                placeholder="GENRES"
+                    style={{ minWidth: "20rem" }}
+                  />
+                </label>
+                <label style={{ fontSize: "18px" }}>
+                  Genres:
+                  <input
+                    placeholder="GENRES"
                     type="text"
                     name="genre"
                     value={editedProduct.genre}
                     onChange={handleInputChange}
-                    style={{minWidth: '20rem'}}
-                />
-            </label>
-            <label style={{fontSize: '18px'}}>Identifier:
-                <input
-                placeholder="IDENTIFIER"
+                    style={{ minWidth: "20rem" }}
+                  />
+                </label>
+                <label style={{ fontSize: "18px" }}>
+                  Identifier:
+                  <input
+                    placeholder="IDENTIFIER"
                     type="text"
                     name="identifier"
                     value={editedProduct.identifier}
                     onChange={handleInputChange}
-                    style={{maxWidth: '8rem'}}
-                />
-            </label>
-            <label style={{fontSize: '18px'}}>Publisher:
-                <input
-                placeholder="PUBLISHER"
+                    style={{ maxWidth: "8rem" }}
+                  />
+                </label>
+                <label style={{ fontSize: "18px" }}>
+                  Publisher:
+                  <input
+                    placeholder="PUBLISHER"
                     type="text"
                     name="publisher"
                     value={editedProduct.publisher}
                     onChange={handleInputChange}
-                    style={{minWidth: '20rem'}}
-                />
-            </label>
-            <label style={{fontSize: '18px'}}>Published date:
-                <input
-                placeholder="PUBLISHED DATE"
+                    style={{ minWidth: "20rem" }}
+                  />
+                </label>
+                <label style={{ fontSize: "18px" }}>
+                  Published date:
+                  <input
+                    placeholder="PUBLISHED DATE"
                     type="text"
                     name="publishedDate"
                     value={editedProduct.publishedDate}
                     onChange={handleInputChange}
-                    style={{maxWidth: '3rem'}}
-                />
-            </label>
-            <label style={{fontSize: '18px'}}>Description:</label>
+                    style={{ maxWidth: "3rem" }}
+                  />
+                </label>
+                <label style={{ fontSize: "18px" }}>Description:</label>
                 <textarea
-                placeholder="DESCRIPTION"
-                    name="description"
-                    value={editedProduct.description}
-                    onChange={handleInputChange}
-                /><br/>
-            <div className="d-flex justify-content-around">
-            <button className={style.cancel} onClick={()=>setIsEditing(false)}>Cancel update</button>
-            <button className={style.update} onClick={handleEditProduct}>Update product</button>
-            </div>
-            </div>)}
+                  placeholder="DESCRIPTION"
+                  name="description"
+                  value={editedProduct.description}
+                  onChange={handleInputChange}
+                />
+                <br />
+                <div className="d-flex justify-content-around">
+                  <button
+                    className={style.cancel}
+                    onClick={() => setIsEditing(false)}
+                  >
+                    Cancel update
+                  </button>
+                  <button className={style.update} onClick={handleEditProduct}>
+                    Update product
+                  </button>
+                </div>
+              </div>
+            )}
 
             {!isEditing && (
               <>
-                 <h1 style={{width: '100%',fontSize: '30px'}}>{title}</h1>
-            <h2 style={{fontSize: '18px'}}>{subtitle}</h2>
-            <p style={{fontSize: '20px'}}>Price: ${price}</p>
-            <p style={{fontSize: '20px'}}>Stock: {stock}</p>
-            <p style={{fontSize: '20px'}}>Author's: {authors}</p>
-            <p style={{fontSize: '20px', maxHeight: '60%', borderRadius: '20px', paddingLeft: '8px', backgroundColor: '#71a6e2'}}>{description}</p>
-            <p style={{width: '50%', fontSize: '20px'}}>Rating global: {averageRating}</p> <p style={{width: '50%', fontSize: '20px'}}></p>
-            <p style={{width: '33.3%', fontSize: '18px'}}>Categories: {genre}</p>
-            <p style={{width: '33.3%', fontSize: '18px'}}>Pages: {pages}</p>
-            <p style={{width: '33.3%', fontSize: '18px'}}>{publisher} : {publishedDate}</p>
-            <p style={{fontSize: '20px'}}>ISBN : {identifier}</p>
+                <h1 style={{ width: "100%", fontSize: "30px" }}>{title}</h1>
+                <h2 style={{ fontSize: "18px" }}>{subtitle}</h2>
+                <p style={{ fontSize: "20px" }}>Price: ${price}</p>
+                <p style={{ fontSize: "20px" }}>Stock: {stock}</p>
+                <p style={{ fontSize: "20px" }}>Author's: {authors}</p>
+                <p
+                  style={{
+                    fontSize: "20px",
+                    maxHeight: "60%",
+                    borderRadius: "20px",
+                    paddingLeft: "8px",
+                    backgroundColor: "#71a6e2",
+                  }}
+                >
+                  {description}
+                </p>
+                <p style={{ width: "50%", fontSize: "20px" }}>
+                  Rating global: {averageRating}
+                </p>{" "}
+                <p style={{ width: "50%", fontSize: "20px" }}></p>
+                <p style={{ width: "33.3%", fontSize: "18px" }}>
+                  Categories: {genre}
+                </p>
+                <p style={{ width: "33.3%", fontSize: "18px" }}>
+                  Pages: {pages}
+                </p>
+                <p style={{ width: "33.3%", fontSize: "18px" }}>
+                  {publisher} : {publishedDate}
+                </p>
+                <p style={{ fontSize: "20px" }}>ISBN : {identifier}</p>
               </>
             )}
-
           </div>
         </div>
-        <Link to="/dashboard" style={{ textDecoration: 'none' }}><p className={style.back}>Go back</p></Link>
+        <Link to="/dashboard" style={{ textDecoration: "none" }}>
+          <p className={style.back}>Go back</p>
+        </Link>
       </div>
     </div>
   );
